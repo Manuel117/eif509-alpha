f0c19a2431db2564ec5e4c331a48572ecfd04783
Manuel117 <manuel.sanabria0589@gmail.com>
2015-09-01 22:12:13 -0600

    Cambios en archivo de hibernate y agregar test de menu
    MenuDaoTest

--- alpha/core/src/main/java/una/cr/alpha/dao/hibernate/MenuDaoHibernate.java
+++ alpha/core/src/main/java/una/cr/alpha/dao/hibernate/MenuDaoHibernate.java
@@ -1,5 +1,22 @@
 package una.cr.alpha.dao.hibernate;
 
-public class MenuDaoHibernate {
+import java.util.List;
 
+import org.appfuse.dao.hibernate.GenericDaoHibernate;
+import org.hibernate.criterion.Restrictions;
+import una.cr.alpha.model.Menu;
+import una.cr.alpha.dao.MenuDao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository("menuDao")
+public class MenuDaoHibernate extends GenericDaoHibernate<Menu, Long> implements MenuDao {
+
+	public MenuDaoHibernate() {
+        super(Menu.class);
+    }
+	
+	 public List<Menu> findById(Long id) {
+	        return getSession().createCriteria(Menu.class).add(Restrictions.eq("id", id)).list();
+	    }
 }
