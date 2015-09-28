bedb100d322a3c2375ba165e441bddbf9dd3e1a9
Manuel117 <manuel.sanabria0589@gmail.com>
2015-09-27 22:49:57 -0600

    cambios

--- alpha/core/src/main/java/una/cr/alpha/dao/hibernate/PreferenceDaoHibernate.java
+++ alpha/core/src/main/java/una/cr/alpha/dao/hibernate/PreferenceDaoHibernate.java
@@ -2,10 +2,12 @@ package una.cr.alpha.dao.hibernate;
 
 import org.appfuse.dao.hibernate.GenericDaoHibernate;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
 
 import una.cr.alpha.dao.PreferenceDao;
 import una.cr.alpha.model.Preference;
 
+@Repository("preferenceDao")
 public class PreferenceDaoHibernate extends GenericDaoHibernate<Preference,Long> implements PreferenceDao {
    public PreferenceDaoHibernate() {
 	        super(Preference.class);
